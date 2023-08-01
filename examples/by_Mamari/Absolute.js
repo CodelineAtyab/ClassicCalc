0b25e47919be084f3936b119d15937c90904982b
mohammed3599 <kataloni1999@gmail.com>
2023-08-01 13:45:01 +0400

    Added Absolute function

---
+++ examples/by_Mamari/Absolute.js
@@ -0,0 +1,12 @@
+function absoluteValue(number) {
+    if (number < 0) {
+      return -number;
+    } else {
+      return number;
+    }
+  }
+  
+  let number = -5;
+  let value = absoluteValue(number);
+  
+  console.log(absoluteValue);
