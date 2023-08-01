b91f47515a789da60ee5998076d46bd119ca3978
WhatIsFun <mohammed110051@gmail.com>
2023-08-01 13:22:47 +0400

    Mohammed Said Examples

---
+++ examples/by_mSaid/myexamples.js
@@ -0,0 +1,20 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+if (hour < 18) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
