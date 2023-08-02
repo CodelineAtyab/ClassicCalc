df25c3a737b6d0c9ac606a7f2a116b9156fdb3f0
ahmedhh33 <ahmedhh3633@gmail.com>
2023-08-02 12:05:40 +0400

    Difference Method

---
+++ prac.js
@@ -0,0 +1,8 @@
+console.log("Difference Method");
+function calculateDifference(num1, num2) {
+    return num1 - num2;
+}
+
+// Example usage:
+let result = calculateDifference(10, 5);
+console.log("Difference:", result); 
