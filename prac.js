54caf313ebcd799498a7e97874783acd589d4a47
mohammedaladhary <caladhary@gmail.com>
2023-08-02 13:19:40 +0400

    added comment by aladhary

--- prac.js
+++ prac.js
@@ -1,10 +1,11 @@
+//multiply method by aladhary
 const calculator = {
     multiply: function(a, b) {
       return a * b;
     },
   };
   
-  // Test the multiply method
+  // Test the method
   const num1 = 4;
   const num2 = 6;
   const result = calculator.multiply(num1, num2);
