2ef6e96a634c3b4294146791dce822ddfbf4685b
alhabsi16 <mxmx8283@gmail.com>
2023-08-02 12:05:53 +0400

    MOD method

--- examples/by_muhanad/myExamples.js
+++ examples/by_muhanad/myExamples.js
@@ -1,11 +1,11 @@
 const calculator = {
-    MOd: function(a, b) {
-      return a % b;
+    ADD: function(a, b) {
+      return a + b;
     },
   };
   
-  // Test the MOD method
+  // Test the add method
   const num1 = 10;
   const num2 = 3;
-  const result = calculator.MOd(num1, num2);
-  console.log(`${num1} % ${num2} = ${result}`);
+  const result = calculator.ADD(num1, num2);
+  console.log(`${num1} + ${num2} = ${result}`);
--- prac.js
+++ prac.js
@@ -1,26 +1,11 @@
-setTimeout(()=>{
-    let myHeading2 = document.getElementById("myMsg");
-    myHeading2.innerHTML = "You are selected.....";
-}, 3000)
-
-let x = 0;
-
-function calcPow(base, exp) {
-    return Math.pow(base, exp);
-}
-
-console.log(calcPow(8,2));
-
-console.log("Hello World");
-
 const calculator = {
-    MOd: function(a, b) {
+    MOD: function(a, b) {
       return a % b;
     },
   };
   
-  // Test the MOD method
+  // Test the add method
   const num1 = 15;
   const num2 = 4;
-  const result = calculator.MOd(num1, num2);
+  const result = calculator.MOD(num1, num2);
   console.log(`${num1} % ${num2} = ${result}`);
