05b8f438d2cc23499130eae5fc33c767164355a3
mohammedaladhary <caladhary@gmail.com>
2023-08-02 13:17:53 +0400

    multiply method

--- prac.js
+++ prac.js
@@ -1,14 +1,11 @@
-setTimeout(()=>{
-    let myHeading2 = document.getElementById("myMsg");
-    myHeading2.innerHTML = "You are selected.....";
-}, 3000)
-
-let x = 0;
-
-function calcPow(base, exp) {
-    return Math.pow(base, exp);
-}
-
-console.log(calcPow(8,2));
-
-console.log("Hello World");
+const calculator = {
+    multiply: function(a, b) {
+      return a * b;
+    },
+  };
+  
+  // Test the multiply method
+  const num1 = 4;
+  const num2 = 6;
+  const result = calculator.multiply(num1, num2);
+  console.log(`${num1} * ${num2} = ${result}`);
